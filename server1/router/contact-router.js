95ee01b6476435d87dc62e18779e8b242f170dc9
Uday Surothiya <112634124+udyThe@users.noreply.github.com>
2024-09-28 14:02:34 +0530

    Delete server1 directory

--- server1/router/contact-router.js
+++
@@ -1,5 +0,0 @@
-const express = require ('express');
-const router = express.Router();
-const {saveContactMessage} = require ('../controllers/contact-controller');
-router.post('/contact' , saveContactMessage);
-module.exports = router;
